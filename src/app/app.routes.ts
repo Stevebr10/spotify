040230888e63921e8ddb81f3bbef16d5b9e39814
Stevebr10 <brandon.onaguaman@epn.edu.ec>
2025-05-02 20:00:51 -0500

    Modulos y Rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    {
+        path: '', //Cuando ponemos en vacio hacemos referencia a localhost:4200
+        
+    },
+    {
+        path: 'hello', //Cuando ponemos en vacio hacemos referencia a localhost:4200
+        
+    }
+];
+
