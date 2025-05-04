cf0b1a98c1bd46400cc4a8e53bd5c454a6cb678d
Stevebr10 <brandon.onaguaman@epn.edu.ec>
2025-05-04 13:38:55 -0500

    LazyLoading

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,11 +4,7 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
     {
         path: '', //Cuando ponemos en vacio hacemos referencia a localhost:4200
-        
-    },
-    {
-        path: 'hello', //Cuando ponemos en vacio hacemos referencia a localhost:4200
-        
+        loadChildren: ()=>import(`./modules/home/home.module`).then(m=>m.HomeModule)
     }
 ];
 
--- src/app/modules/home/home-routing.module.ts
+++ src/app/modules/home/home-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './page/home-page/home-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',   //Para ingresar el path seria -> http://localhost:4200/home/dashboard en caso de que path estuviese con dashboard y el padre con home
+    component: HomePageComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
