017f8e100c27bfcd34ceaafea8d74740a46f5f15
Stevebr10 <brandon.onaguaman@epn.edu.ec>
2025-05-05 22:00:04 -0500

    Iconos

--- src/app/shared/components/side-bar/side-bar.component.ts
+++ src/app/shared/components/side-bar/side-bar.component.ts
@@ -1,13 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common'; // Añade esta línea
 
 @Component({
   selector: 'app-side-bar',
   standalone: true, // Asegúrate que esto esté presente
-  imports: [], // Agrega aquí otros componentes/directivas/pipes que necesites
+  imports: [CommonModule], // Agrega aquí otros componentes/directivas/pipes que necesites
   templateUrl: './side-bar.component.html',
   styleUrl: './side-bar.component.css'
 })
 export class SideBarComponent implements OnInit {
+  linksMenu:Array<any> = [
+    {
+      name: 'Home',
+      icon: 'uil-estate'
+    },
+    {
+      name: 'Buscar',
+      icon: 'uil-estate'
+    }
+  ]
 
   constructor(){}
 
