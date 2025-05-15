cc39fc9bb5bc6afafbae287acb71bdbea24fc120
Stevebr10 <brandon.onaguaman@epn.edu.ec>
2025-05-15 15:25:30 -0500

    Cf. header and body

---
+++ src/app/shared/components/play-list-body/play-list-body.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-play-list-body',
+  imports: [],
+  templateUrl: './play-list-body.component.html',
+  styleUrl: './play-list-body.component.css'
+})
+export class PlayListBodyComponent {
+
+}
