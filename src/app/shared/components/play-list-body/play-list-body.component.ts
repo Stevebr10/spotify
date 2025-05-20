aaa95a58aea70bd228f91cafac973d1b7ecc0a9c
Stevebr10 <brandon.onaguaman@epn.edu.ec>
2025-05-19 21:06:45 -0500

    Implementacion de orderlist pipe

--- src/app/shared/components/play-list-body/play-list-body.component.ts
+++ src/app/shared/components/play-list-body/play-list-body.component.ts
@@ -2,11 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import * as dataRaw from '../../../data/tracks.json'
 import { TrackModel } from '@core/models/tracks.model';
 import { CommonModule } from '@angular/common'; // Añade esta línea
+import { OrderlistPipe } from '@shared/pipe/orderlist.pipe';
 
 @Component({
   selector: 'app-play-list-body',
   standalone: true, // Asegúrate que esto esté presente
-  imports: [CommonModule],
+  imports: [CommonModule, OrderlistPipe],
   templateUrl: './play-list-body.component.html',
   styleUrl: './play-list-body.component.css'
 })
--- src/app/shared/pipe/orderlist.pipe.ts
+++ src/app/shared/pipe/orderlist.pipe.ts
@@ -1,12 +1,16 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { TrackModel } from '@core/models/tracks.model';
 
 @Pipe({
   name: 'orderlist'
 })
 export class OrderlistPipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(value: TrackModel[]): TrackModel[] {
+    
+    console.log('👉', value)
+    
+    return [];
   }
 
 }
