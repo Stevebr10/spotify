e3bd181082cdc58ffbc0d0a172e14cc151681d2b
Stevebr10 <brandon.onaguaman@epn.edu.ec>
2025-06-02 17:45:41 -0500

    ImgBroken

--- src/app/shared/directives/img-broken.directive.ts
+++ src/app/shared/directives/img-broken.directive.ts
@@ -1,10 +1,55 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, HostListener } from '@angular/core';
+
 
 @Directive({
-  selector: '[appImgBroken]'
+  selector: 'img[appImgBroken]',
+  standalone: true
 })
 export class ImgBrokenDirective {
+  //host Host HOST
+  @HostListener('error') handleError(): void{
+    const elNative = this.elHost.nativeElement;
+    console.log('🔴 Esta imagen revento ->', this.elHost);
+    elNative.src='images/imgbroken.jpg'
+    //elNative.src='https://i.pinimg.com/736x/69/d5/3e/69d53ef9520b57a5e2af1b1387807fc7.jpg'
+  }
 
-  constructor() { }
+  constructor(private elHost: ElementRef) { 
+    console.log(this.elHost);
+  }
 
 }
+
+// import { Directive, ElementRef, HostListener, Input } from '@angular/core';
+
+// @Directive({
+//   selector: 'img[appImgBroken]',
+//   standalone: true
+// })
+// export class ImgBrokenDirective {
+  
+//   @Input() appImgBroken: string = 'https://i.pinimg.com/736x/69/d5/3e/69d53ef9520b57a5e2af1b1387807fc7.jpg';
+//   private hasError: boolean = false;
+
+//   constructor(private elHost: ElementRef) {
+//     console.log('🟢 Directiva ImgBroken inicializada');
+//   }
+
+//   @HostListener('error', ['$event']) 
+//   handleError(event: any): void {
+//     console.log('🔴 ERROR detectado en imagen');
+//     console.log('🔴 Src original:', event.target.src);
+    
+//     if (!this.hasError) {
+//       this.hasError = true;
+//       const elNative = this.elHost.nativeElement;
+//       console.log('🔴 Cambiando src a:', this.appImgBroken);
+//       elNative.src = this.appImgBroken;
+//     }
+//   }
+
+//   @HostListener('load', ['$event'])
+//   handleLoad(event: any): void {
+//     console.log('🟢 Imagen cargada correctamente:', event.target.src);
+//   }
+// }
