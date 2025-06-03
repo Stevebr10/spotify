fc9cf597847609bf883c25a13c0f13c662332dd0
Stevebr10 <brandon.onaguaman@epn.edu.ec>
2025-06-02 21:21:56 -0500

    Login

--- src/app/modules/auth/pages/login-page/login-page.component.ts
+++ src/app/modules/auth/pages/login-page/login-page.component.ts
@@ -1,21 +1,43 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';  // Importa CommonModule
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 @Component({
   selector: 'app-login-page',
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   standalone: true,
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.css'
 })
 export class LoginPageComponent implements OnInit {
   
+  formLogin: FormGroup = new FormGroup({});
+
   constructor(){
 
   }
 
-  ngOnInit(): void {
-      
+  ngOnInit(): void { //Ciclo de vida principal
+      this.formLogin = new FormGroup(
+        {
+          email: new FormControl('',[
+            Validators.required,  //validadcion para que exista algo
+            Validators.email      // validadcion para que lo ingresado sea un email
+          ]),
+          password: new FormControl('',[
+            Validators.required,
+            Validators.minLength(6),
+            Validators.maxLength(12)
+          ]),
+        }
+      )
   }
+
+  sendLogin():void{
+    const body = this.formLogin.value
+    console.log('💥💥💥', body);
+  }
+
 }
