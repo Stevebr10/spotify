fa2c9a76a7e2930ef478df36890e10f62f04db3c
Stevebr10 <brandon.onaguaman@epn.edu.ec>
2025-06-03 16:26:18 -0500

    Sucripcion y Desuscripcion

--- src/app/shared/components/card-player/card-player.component.ts
+++ src/app/shared/components/card-player/card-player.component.ts
@@ -2,6 +2,7 @@ import { Component, Input, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';  // Importa CommonModule
 import { TrackModel } from '@core/models/tracks.model';
 import { ImgBrokenDirective } from '@shared/directives/img-broken.directive';
+import { MultimediaService } from '@shared/services/multimedia.service';
 
 
 @Component({
@@ -17,12 +18,17 @@ export class CardPlayerComponent implements OnInit{
   @Input() mode: 'small' | 'big' = 'small';
   @Input() track: TrackModel = {_id:0, name:'', album:'', url:'', cover:''};
 
-  constructor(){
+  constructor(private multimediaService: MultimediaService){
 
   }
 
   ngOnInit(): void {
       
+  }
+
+  sendPlay(track: TrackModel): void{
+    //console.log('Enviando cancion al reproductor')
+    this.multimediaService.callback.emit(track)
   }
 
 }
--- src/app/shared/components/media-player/media-player.component.ts
+++ src/app/shared/components/media-player/media-player.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';  // Importa CommonModule
 import { TrackModel } from '@core/models/tracks.model';
+import { MultimediaService } from '@shared/services/multimedia.service';
+import { Subscription } from 'rxjs'; //Programacion reactiva
 
 @Component({
   selector: 'app-media-player',
@@ -9,7 +11,7 @@ import { TrackModel } from '@core/models/tracks.model';
   templateUrl: './media-player.component.html',
   styleUrl: './media-player.component.css'
 })
-export class MediaPlayerComponent implements OnInit {
+export class MediaPlayerComponent implements OnInit, OnDestroy {
 
   mockCover: TrackModel = {
     //informacion sobre la barra de progreso de la cancion 
@@ -20,10 +22,22 @@ export class MediaPlayerComponent implements OnInit {
     _id: 1
   }
 
-  constructor(){}
+  listObservers$: Array<Subscription>=[]
+
+  constructor(private multimediaService: MultimediaService){}
 
   ngOnInit(): void {
-      
+    const observer1$: Subscription = this.multimediaService.callback.subscribe(
+      (response: TrackModel) => {
+        console.log('Reciviendo una cancion....', response);
+      }
+    )
+    this.listObservers$ = [observer1$]
+  }
+
+  ngOnDestroy(): void {
+    this.listObservers$.forEach(u => u.unsubscribe())
+    console.log('🔴🔴🔴🔴 BOOOMMM!!!!')
   }
 
 }
