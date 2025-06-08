c19e5768fb7d63cfb6db1511cbceb4a1081f86f8
Stevebr10 <brandon.onaguaman@epn.edu.ec>
2025-06-07 23:51:18 -0500

    Comentarios Servicios

--- src/app/shared/services/multimedia.service.ts
+++ src/app/shared/services/multimedia.service.ts
@@ -4,7 +4,7 @@ import { EventEmitter, Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class MultimediaService {
-
+  // Documentar el funcionamiento de los servicion y en donde son incorporados
   callback: EventEmitter<any> = new EventEmitter<any>() //Esto es publico
 
 
