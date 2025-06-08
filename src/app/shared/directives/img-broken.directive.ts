81003337c350b5d124dc66ac67048019bbf8a7df
Stevebr10 <brandon.onaguaman@epn.edu.ec>
2025-06-07 23:49:41 -0500

    Clean Code

--- src/app/shared/directives/img-broken.directive.ts
+++ src/app/shared/directives/img-broken.directive.ts
@@ -21,37 +21,3 @@ export class ImgBrokenDirective {
   }
 
 }
-
-// import { Directive, ElementRef, HostListener, Input } from '@angular/core';
-
-// @Directive({
-//   selector: 'img[appImgBroken]',
-//   standalone: true
-// })
-// export class ImgBrokenDirective {
-  
-//   @Input() appImgBroken: string = 'https://i.pinimg.com/736x/69/d5/3e/69d53ef9520b57a5e2af1b1387807fc7.jpg';
-//   private hasError: boolean = false;
-
-//   constructor(private elHost: ElementRef) {
-//     console.log('🟢 Directiva ImgBroken inicializada');
-//   }
-
-//   @HostListener('error', ['$event']) 
-//   handleError(event: any): void {
-//     console.log('🔴 ERROR detectado en imagen');
-//     console.log('🔴 Src original:', event.target.src);
-    
-//     if (!this.hasError) {
-//       this.hasError = true;
-//       const elNative = this.elHost.nativeElement;
-//       console.log('🔴 Cambiando src a:', this.appImgBroken);
-//       elNative.src = this.appImgBroken;
-//     }
-//   }
-
-//   @HostListener('load', ['$event'])
-//   handleLoad(event: any): void {
-//     console.log('🟢 Imagen cargada correctamente:', event.target.src);
-//   }
-// }
