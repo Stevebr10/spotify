f7a858797d3b5aa5019968b1cccc330624386eb9
Stevebr10 <brandon.onaguaman@epn.edu.ec>
2025-06-23 23:51:56 -0500

    Environment not prod

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,3 +1,4 @@
+//Entorno para back, se diferenica con el anterior ya que es de produccion 
 export const environment = {
   production: false,
   api: 'http://localhost:3001/api/1.0'  // cambia esta URL según tu backend local
