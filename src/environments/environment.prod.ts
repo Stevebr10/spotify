5b1dcbedcc574426b23b0134bc0ba68b8274fbf2
Stevebr10 <brandon.onaguaman@epn.edu.ec>
2025-06-23 23:50:44 -0500

    Environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,6 @@
+//Creacion de Entornos para el back con api
 export const environment = {
   production: false,
   api: 'http://localhost:3001/api/1.0'  // cambia esta URL según tu backend local
+
 };
